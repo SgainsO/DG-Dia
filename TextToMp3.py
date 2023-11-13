b784350c2dbacc1dfb55b96db01ac76b2469d3a7
sgainso <jptjasc@gmail.com>
2023-11-13 09:58:09 -0500

    initial

---
+++ TextToMp3.py
@@ -0,0 +1,63 @@
+import os
+import shutil
+import requests
+import elevenlabs
+from bs4 import BeautifulSoup
+
+class item:
+    def __init__(self, name: str, amount: int):
+        self.name = name
+        self.amount = amount
+
+    name: str
+    amount: int
+
+store_pages = []
+
+
+def EmptyDirec(folder):
+    for file in os.listdir(folder):
+        os.remove(f"out/{file}")
+
+def GetVoice(TextInput, Name):
+    url = "https://api.elevenlabs.io/v1/text-to-speech/zrHiDhphv9ZnVXBqCLjz/stream"
+    nMp3 = 0                     #this long string is an example
+    present = False
+    for i in store_pages:
+        if Name == i.name:
+            nMp3 = i.amount
+            i.amount += 1
+            present = True                                           #of a voice id
+
+    if present == False:
+        newItem = item(name = Name, amount = 0)
+        store_pages.append(newItem)
+
+
+    CHUNK_SIZE = 1024
+    headers ={
+        "Accept": "audio/mpeg",
+        "Content-Type": "application/json",
+        "xi-api-key": "PUT KEY HERE"           #Put your API key here
+    }
+
+    data ={
+        "text": TextInput,                 #Text input will contain the wikipedia Paragraph
+        "model_id": "eleven_monolingual_v1",
+        "voice_settings": {
+            "stability": 0.5,
+            "similarity_boost": 0.5
+            }
+        }
+    response = requests.post(url, json=data, headers=headers, stream=True)
+    with open(f"making/{Name}{nMp3}.mp3", 'wb') as f:
+        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+            if chunk:
+                f.write(chunk)
+        shutil.move(f"making/{Name}{nMp3}.mp3", 'out', copy_function = shutil.copytree)
+
+
+
+def DownloadAllFromWiki(wiki, name):
+    for tag in wiki:
+        GetVoice(tag.text, name)
--- main.py
+++ main.py
@@ -1,26 +1,44 @@
 import os
-import requests
+import requests 
+import threading
+from TextToMp3 import *
 from bs4 import BeautifulSoup
+class page:
+    def __init__(self, n, tag, play):
+        self.name = n
+        self.tag = tag
+        self.amPlayed = play
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
+    name : str
+    amOfTag: int
+    amPlayed: int
 
 
-url = "https://en.wikipedia.org/wiki/%C3%85bo_Bloodbath"
+
+
+Topic_Name = "Destiny_(streamer)"
+EmptyDirec("out")
+url = "https://en.wikipedia.org/wiki/" + Topic_Name
 
 response = requests.get(url)
+
 
 
 s = BeautifulSoup(response.text, 'html.parser')
 
 wiki_text = s.find_all(['p', 'h2'])
+lengthOfWiki = len(wiki_text)
+vdT = threading.Thread(target = DownloadAllFromWiki, args=(wiki_text, Topic_Name))
 
-print(wiki_text)
-print(len(wiki_text[1]))
+vdT.start()
 
-
-
-if len(wiki_text[1]) < 10:
-    print("this page a kinda short, is their anything else on the previous section ")
-
-for tag in wiki_text:
-    print(tag.text)
+pgS = []                    #this will be treated as a stack to
+pgS.append(page(Topic_Name, len(wiki_text), 0))
+while True:
+    file_path = "out/" + f"{pgS[-1].name}{pgS[-1].amPlayed}.mp3"
+    if os.path.exists(file_path):
+        os.startfile(file_path)
+        pgS[-1].amPlayed+=1
+        input("")
+    else:
+        print("Voice is loading...")
