0725e361e576f3e2857bafd34d8212edc4e872a8
sgainso <jptjasc@gmail.com>
2023-11-16 13:05:24 -0500

    Update

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import requests 
 import threading
-from TextToMp3 import *
+from TextTo11Lab import *
+from TextToVoice import *
 from bs4 import BeautifulSoup
 class page:
     def __init__(self, n, tag, play):
@@ -35,10 +36,10 @@ vdT.start()
 pgS = []                    #this will be treated as a stack to
 pgS.append(page(Topic_Name, len(wiki_text), 0))
 while True:
-    file_path = "out/" + f"{pgS[-1].name}{pgS[-1].amPlayed}.mp3"
+    file_path = "out/" + f"{pgS[-1].name}{pgS[-1].amPlayed}.wav"
     if os.path.exists(file_path):
         os.startfile(file_path)
         pgS[-1].amPlayed+=1
-        input("")
+        input("-----------Press Any Button To Load the next text ")
     else:
         print("Voice is loading...")
